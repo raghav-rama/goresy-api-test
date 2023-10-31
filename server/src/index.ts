440a90db6fd5db8f0bcb3c62d528048d9b4afa4a
raghav-rama <hackerer528@gmail.com>
2023-10-31 21:55:59 +0530

    fix(server): CORS error

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,14 +3,23 @@ import apiRoutes from './routes';
 import 'dotenv/config';
 import cors from 'cors';
 
-const corsOptions = {
-  origin: 'http://localhost:5173',
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: 'http://localhost:5173',
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
 const app = express();
 
-app.use(cors(corsOptions));
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
